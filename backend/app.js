6c8158b8c53e156c5ba584fd9d44912212c5b673
nejc-k <nejc.kolmanko@student.um.si>
2024-11-02 11:07:07 +0100

    Simple backend authentication

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require("mongoose")
+var passport = require("passport")
+var session = require("express-session")
+const dotenv = require("dotenv").config()
+require("./strategies/local-strategy.js")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,8 +20,73 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connect("mongodb://localhost:27017/meme-generator")
+// .then(()=>{
+// })
+
+
+app.use(
+  session({
+    secret:process.env["EXPRESS_SESSION_SECRET"],
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+      maxAge:60000*60,
+     
+    }
+  })
+)
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post("/api/auth",
+  passport.authenticate("local"),
+  (req,res) =>{
+    
+    res.sendStatus(200)
+  }
+)
+
+app.get("/api/auth/status",
+  (req,res) =>{
+    if(req.user) return res.send({username:req.user.username, credits:req.user.credits});
+
+    return res.sendStatus(401);
+  }
+)
+
+app.post("/api/auth/logout", (req,res)=>{
+  if(!req.user) return res.sendStatus(401);
+  req.logout((err)=>{
+    if(err)return res.sendStatus(400);
+    res.sendStatus(200)
+  })
+})
+
+/* EXAMPLE OF A PROTECTED PATH */
+
+// app.get("/hello", checkAuthenticated, 
+//   (req,res) =>{
+//     if(req.user) return res.send({msg:"Im authenticated!"});
+
+//     return res.sendStatus(401);
+//   }
+// )
+
+
+function checkAuthenticated(req, res,next){
+  req.isAuthenticated() ? next() : res.redirect("/login");
+}
+
+
+app.get("/", (req, res) => {
+  const { session } = res.locals
+  res.render("index", { user: session?.user })
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,7 +101,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+  res.json({messsage:err.message})
 });
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require("mongoose")
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const {username} = req.params
+  const user = await User.findOne({username:username})
+  console.log(user)
+  res.send(user);
+  // res.send('respond with a resource');
+
 });
+
+// router.get('/:username', async function(req, res, next) {
+//   const {username} = req.params
+//   const user = await User.findOne({username:username})
+//   console.log(":uname")
+//   res.send(user);
+// });
+
+router.post("/", async(req,res,next)=>{
+  try {
+    const user = User.findOne({username: req.body.username})
+    if(user) return res.status(400).json({message: "User with this username already exists"});
+
+    bcrypt.genSalt(10, (err,salt) =>{
+      if(err){
+        throw new Error("Error with creating user");
+      }
+      const password = req.body.password
+      bcrypt.hash(password, salt, async(err,hash) => {
+      if(err){
+        throw new Error("Error with creating user");
+      }
+
+        const user = await User.create(
+          {
+            username:req.body.username,
+            password:hash,
+            credits:10
+          }
+
+        )
+        if(!user) throw new Error("Error creating user");
+        res.status(201).json({userId: user.id})
+    } )
+
+    })
+
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message:error.message})
+  }
+})
 
 module.exports = router;
